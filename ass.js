cdf20f360ac9bc21fd0959e42293e29186a0e589
TAKASE Hiroyuki <takase-hiroyuki@users.noreply.github.com>
2026-07-05 03:22:52 +0900

    Add debug message display to HTML

--- ass.js
+++ ass.js
@@ -1,1 +1,16 @@
+// app.js
+import { testMessage } from './config.js';
+import { storageMessage } from './storage.js';
 
+// HTMLのタイトル（h1）の下あたりに、確認メッセージを強制的に追加して表示します
+const debugDiv = document.createElement('div');
+debugDiv.style.padding = '10px';
+debugDiv.style.background = '#e2f0d9';
+debugDiv.innerHTML = `
+    <p>【連動テスト結果】</p>
+    <ul>
+        <li>${testMessage}</li>
+        <li>${storageMessage}</li>
+    </ul>
+`;
+document.body.insertBefore(debugDiv, document.body.firstChild);
