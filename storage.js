2c2e5bf1c4ccd27241ad01e1686f0358f4f0d991
TAKASE Hiroyuki <takase-hiroyuki@users.noreply.github.com>
2026-07-05 05:57:37 +0900

    Add storage functions for saving and retrieving values

--- storage.js
+++ storage.js
@@ -1,2 +1,11 @@
 // storage.js
-export const storageMessage = "storage.js の読み込みに成功しました！";
+
+// 1. 記憶ポケットに「値を保存する」ための関数
+export function saveToStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+// 2. 記憶ポケットから「値を取り出す」ための関数
+export function getFromStorage(key) {
+    return localStorage.getItem(key);
+}
